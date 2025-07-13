b6aeb8cfeadf87ce4e29186a82d1d9544d25b0c3
Jeff Matthew Molina <mm35molina@gmail.com>
2025-07-13 10:32:21 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-tr from-pink-500 to-yellow-300 flex flex-col items-center justify-center gap-4">
+      <h1 className="text-5xl font-bold text-white">🌤️ Tailwind Works!</h1>
+      <p className="text-lg text-white bg-black bg-opacity-30 px-4 py-2 rounded-xl shadow-lg">
+        If you see this colorful screen, you're good to go!
+      </p>
+    </div>
+  )
+}
+
+export default App
